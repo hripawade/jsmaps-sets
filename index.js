32b6cd6b161587a0aa91a490ac57586b405b4105
hripawade <111070997+hripawade@users.noreply.github.com>
2022-11-08 11:15:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// let STR=new Set(["a","b","c","a","d","e","e","c","f","b"]);
+
+// console.log(...STR);
+
+// let smn="abcadeecfb";
+// let map=new Map();
+// let len=smn.length;
+// for(let i=0;i<len;i++){
+//     let char=smn.charAt(i);
+//     if (map.has(char)){
+//         let value=map.get(char);
+//         map.set(char,value+1);
+//     }else{
+//         map.set(char,1);
+//     }
+// }
+// console.log(map);
